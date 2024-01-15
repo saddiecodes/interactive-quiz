4074a11c91e477759816209ea1f4614933e59755
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-15 17:50:11 +0000

    adding functions to the start button

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,40 +1,40 @@
 // Define quiz questions
 const quizQuestions = [
-{
-    question: "What is JavaScript?", 
-    options: ["A programming language", "A type of coffeee", "A new dance move"],
-    correctAnswer: "A programming language", 
-},
-//Add more questions 
-{
-    question: "Commonly used data types DO NOT include:",
-    options: ["strings", "booleans","alerts","numbers"],
-    correctAnswer: "booleans"
-},
+    {
+        question: "What is JavaScript?",
+        options: ["A programming language", "A type of coffeee", "A new dance move"],
+        correctAnswer: "A programming language",
+    },
+    //Add more questions 
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "booleans"
+    },
 
-{
-    question: "The condition in an if/else statement is enclosed within",
-    options: ["quotes", "curly brackets", "parentheses", "sqaure brackets"],
-    correctAnswer: "parentheses",
-},
+    {
+        question: "The condition in an if/else statement is enclosed within",
+        options: ["quotes", "curly brackets", "parentheses", "sqaure brackets"],
+        correctAnswer: "parentheses",
+    },
 
-{
-    question: "Arrays in JavaScript can be used to store",
-    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswer: "all of the above", 
-},
+    {
+        question: "Arrays in JavaScript can be used to store",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above",
+    },
 
-{
-    question: "String values must be enclosed within when being assigned to variables",
-    options: ["commas", "curly brackets", "quotes", "parentheses"],
-    correctAnswer: "curly brackets",
-},
+    {
+        question: "String values must be enclosed within when being assigned to variables",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: "curly brackets",
+    },
 
-{
-    question: "A very useful tool during development and debugging for printing content to the debugger is:",
-    options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    correctAnswer: "console.log", 
-}
+    {
+        question: "A very useful tool during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correctAnswer: "console.log",
+    }
 
 ];
 
@@ -45,12 +45,19 @@ let timeLeft = 60; //set initial time
 
 //Function to start the quiz
 function startQuiz() {
-// Start the timer
-timer = setInterval(function () {
-    timeLeft--; 
+    // Start the timer
+    timer = setInterval(function () {
+        timeLeft--;
 
-    // Check if the game is over
-    if (timeLeft <=0 || currentQuestionIndex === quizQuestions.length) {
-        endgame();
-     }
-    }, 1000); 
+        // Check if the game is over
+        if (timeLeft <= 0 || currentQuestionIndex === quizQuestions.length) {
+            endgame();
+        }
+    }, 1000);
+}; 
+
+var startButton = document.getElementById("start");
+startButton.addEventListener("click",function(){
+    console.log("start button")
+    startQuiz()
+} )
