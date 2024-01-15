1dfad67d532b40a87cebef3c040e564c5c64b929
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-15 10:15:39 +0000

    created javascript sheet and added first question for the quiz

---
+++ assets/js/scores.js
@@ -0,0 +1,9 @@
+// Define quiz questions
+const quizQuestions = [
+{
+    question: "What is JavaScript?", 
+    options: ["A programming language", "A type of coffeee", "A new dance move"],
+    correctAnswer: "A programming language", 
+},
+//Add more questions 
+];
