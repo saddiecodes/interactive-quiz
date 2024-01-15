80fb492ade7efcb46255857d865798439268edc1
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-15 10:35:03 +0000

    addition of questions with multiple choice options to javascript

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -6,4 +6,23 @@ const quizQuestions = [
     correctAnswer: "A programming language", 
 },
 //Add more questions 
+{
+    question: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans","alerts","numbers"],
+    correctAnswer: "booleans"
+},
+
+{
+    question: "The condition in an if/else statement is enclosed within",
+    options: ["quotes", "curly brackets", "parentheses", "sqaure brackets"],
+    correctAnswer: "parentheses",
+},
+
+{
+    question: "Arrays in JavaScript can be used to store",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "all of the above", 
+},
+
+
 ];
