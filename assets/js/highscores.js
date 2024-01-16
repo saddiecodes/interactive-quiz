6c43706e1ffeb256fde97366672bc6d11c565214
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-16 23:25:09 +0000

    fixing error codes in the high score js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -8,7 +8,7 @@ var timerEl = document.getElementById('time');
 var choicesEl = document.getElementById('choices');
 var submitBtn = document.getElementById('submit');
 var startBtn = document.getElementById('start');
-var nameEl = document.getElementById('name');
+var nameEl = document.getElementById('initials');
 var feedbackEl = document.getElementById('feedback');
 
 
@@ -31,12 +31,12 @@ function getQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
 
   var titleEl = document.getElementById('question-title');
-  titleEl.textContent = currentQuestion.title;
+  titleEl.textContent = currentQuestion.question;
 
   choicesEl.innerHTML = '';
 
-  for (var i = 0; i < currentQuestion.choices.length; i++) {
-    var choice = currentQuestion.choices[i];
+  for (var i = 0; i < currentQuestion.options.length; i++) {
+    var choice = currentQuestion.options[i];
     var choiceNode = document.createElement('button');
     choiceNode.setAttribute('class', 'choice');
     choiceNode.setAttribute('value', choice);
@@ -49,17 +49,22 @@ function getQuestion() {
 
 function questionClick(event) {
   var buttonEl = event.target;
-}
+
   if (!buttonEl.matches('.choice')) {
     return;
   }
-  if (buttonEl.value !== questions[currentQuestionIndex].answer) {
+  if (buttonEl.value !== questions[currentQuestionIndex].correctAnswer) {
     time -= 15;
 
     if (time < 0) {
       time = 0;
     }
     timerEl.textContent = time; 
+    feedbackEl.textContent = 'Wrong!';
+  } else {
+
+    feedbackEl.textContent = 'Correct!';
+  }
 
   feedbackEl.setAttribute('class', 'feedback');
   setTimeout(function () {
