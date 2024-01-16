7c4b66cee7f9b3690845fdb27eb2ae9e1090bbf4
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-16 19:55:10 +0000

    adding question.js and questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+
+    {
+                question: 'What is JavaScript?',
+                options: ['A programming language', 'A type of coffee', 'A new dance move'],
+                correctAnswer: 'A programming language',
+            },
+    {
+                question: 'Commonly used data types DO NOT include:',
+                options: ['strings', 'booleans', 'alerts', 'numbers'],
+                correctAnswer: 'booleans'
+    },
+
+    {
+                question: 'The condition in an if/else statement is enclosed within',
+                options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+                correctAnswer: 'parentheses',
+    },
+
+    {
+                question: 'Arrays in JavaScript can be used to store',
+                options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+                correctAnswer: 'all of the above',
+    },
+
+    {
+                question: 'String values must be enclosed within when being assigned to variables',
+                options: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+                correctAnswer: 'curly brackets',
+    },
+
+    {           question: 'A very useful tool during development and debugging for printing content to the debugger is:',
+                options: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+                correctAnswer: 'console.log',
+    }, 
+
+];
